52cbea9b01ba0522af6d9a6c71bb2202005187bf
Denis <dr144371@gmail.com>
2024-06-01 18:30:59 +0300

    Add deleting a task

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,52 @@ import {TodoList} from "./todoList.js";
 
 const input = document.querySelector(".input");
 const tasksWrapper = document.querySelector(".tasks-wrapper");
+const controlsNumber = document.querySelector(".controls__number");
 const todoList = new TodoList(".tasks-wrapper");
 
 const addTask = () => {
-    todoList.addTask(input.value);
-    input.value = "";
+    if (input.value.length !== 0) {
+        todoList.addTask(input.value);
+        input.value = "";
+    }
+}
 
-    console.log(todoList.tasks);
+const updateTasksCount = () => {
+    controlsNumber.textContent = String(todoList.tasks.length);
+}
+
+const toggleTaskActivity = (elem) => {
+    const elemId = Number(elem.dataset.id);
+    todoList.toggleActive(elemId);
+    elem.classList.toggle("task--finished");
+}
+
+const deleteTask = (elem) => {
+    const elemId = Number(elem.dataset.id);
+    todoList.removeTask(elemId);
 }
 
 input.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         addTask();
+        updateTasksCount();
     }
 })
 
 tasksWrapper.addEventListener("click", (event) => {
     const elem = event.target;
+    const task = elem.closest(".task");
     if (elem.classList.contains("checkbox")) {
+        toggleTaskActivity(task);
+    } else if (elem.classList.contains("delete")) {
+        deleteTask(task);
+    }
+})
+
+tasksWrapper.addEventListener("dblclick", (event) => {
+    const elem = event.target;
+    if (elem.classList.contains("title")) {
         const elemId = elem.closest(".task").dataset.id;
-        todoList.toggleActive(Number(elem));
+        todoList.changeTitle(Number(elemId));
     }
 })
--- assets/js/todoList.js
+++ assets/js/todoList.js
@@ -15,17 +15,31 @@ export class TodoList {
         this.renderTasks();
     }
 
+    removeTask(taskId) {
+        this.tasks = this.tasks.filter(task => task.id !== taskId);
+        this.renderTasks();
+    }
+
     toggleActive(taskId) {
         const todo = this.tasks.find(task => task.id === taskId);
         todo.isActive = !todo.isActive;
-        console.log(this.tasks);
+    }
+
+    changeTitle(taskId, newTitle) {
+        const todo = this.tasks.find(task => task.id === taskId);
+        todo.title = newTitle;
     }
 
     renderTasks() {
         const tasks = this.tasks.map(task =>
             `<div class="task" data-id="${task.id}">
                 <input class="task__checkbox checkbox" type="checkbox"> 
-                <p class="task__title title" data-id="${task.id}">${task.title}</p>
+                <div class="task__inner">
+                    <p class="task__title title" data-id="${task.id}">${task.title}</p>
+                    <button class="task__btn">
+                        <img class="delete" src="assets/icons/cross.svg" alt="delete icon">
+                    </button>
+                </div>
             </div>`
         );
         this.taskWrapper.innerHTML = tasks.join("");
